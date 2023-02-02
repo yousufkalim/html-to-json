3602dfc6e1f0f048e12c374e3998061b2890fef4
Yousuf Kalim <yousufkalim@outlook.com>
2023-02-02 12:10:27 +0500

    Added test case

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals';
-import HTMLParser from '../src';
+import HTMLParser, { JSONToHTML } from '../src';
 
 test('Test the parser by converting HTML to JSON', async () => {
   const element = '<div><ul><li>Hello <strong>World</strong></li></ul></div>';
@@ -7,5 +7,15 @@ test('Test the parser by converting HTML to JSON', async () => {
 
   const expected =
     '{"type":"div","content":[{"type":"ul","content":[{"type":"li","content":["Hello ",{"type":"strong","content":["World"]}]}]}]}';
+  expect(result).toMatch(expected);
+});
+
+test('Test the parser by converting JSON to HTML', async () => {
+  const element =
+    '{"type":"div","content":[{"type":"ul","content":[{"type":"li","content":["Hello ",{"type":"strong","content":["World"]}]}]}]}';
+  const result = await JSONToHTML(element, true);
+
+  const expected = '<div><ul><li>Hello <strong>World</strong></li></ul></div>';
+
   expect(result).toMatch(expected);
 });
